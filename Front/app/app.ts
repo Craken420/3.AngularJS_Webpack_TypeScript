3460e95ca82f9091b7f58cf56934e7b04935f9ce
Luis Angel Peña <lapena@mavi.mx>
2021-03-17 14:44:24 -0600

    feat:: Add the service to get the users data

--- Front/app/app.ts
+++ Front/app/app.ts
@@ -2,6 +2,9 @@ import { module, element, bootstrap, ILogService } from 'angular';
 import '@uirouter/angularjs';
 import { AppComponent } from '../app/app.component';
 import { HomeComponent } from '../app/home/home.component';
+import { UserComponent } from '../app/user/user.component';
+import { UserService } from '../app/service/user.services';
+
 import './app.less';
 
 export let app = module('app', [
@@ -17,11 +20,18 @@ export let app = module('app', [
             name: 'app.home',
             url: '/home',
             component: HomeComponent.NAME
+        }).state({
+            name: 'app.user',
+            url: '/user?id',
+            component: UserComponent.NAME,
         });
+
         $urlRouterProvider.otherwise('/app');
     }])
     .component(AppComponent.NAME, new AppComponent())
-    .component(HomeComponent.NAME, new HomeComponent());
+    .component(HomeComponent.NAME, new HomeComponent())
+    .component(UserComponent.NAME, new UserComponent())
+    .service(UserService.NAME, UserService);
 
 element(document).ready(() => {
     bootstrap(document, ['app']);
